9f7d6aa41106b8cddfe01cbdb98c83e549dfa267
jose <jose.fernando.botina>
2023-01-27 21:03:37 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,65 +1,54 @@
-const { ApolloServer, gql } = require("apollo-server");
+import {ApolloServer, gql} from "apollo-server";
 
 
 const typeDefs = gql`
-  type Book {
-    id: String  
-    title: String
+  type breakingquote {
+    quote: String  
     author: String
   }
 
   type Query {
-    Getbooks: [Book],
-    Getbook(id:String!):[Book]
+    Getbreakingquotes: [breakingquote],
+    Getbreakingquote(quote:String!):[breakingquote]
   }
   type Mutation {
-      CreateBook(id: String!,title: String!, author: String!): Book
-      DeleteBook(id: String!): Book
-      UpdateBook(id: String!,title: String!, author: String!): Book 
+      Createbreakingquote(quote: String!, author: String!): breakingquote
+      Deletebreakingquote(quote: String!): breakingquote
+      Updatebreakingquote(quote: String!, author: String!): breakingquote 
   }
 `;
 
-let books = [
-    {
-      id:"1",
-      title: 'The Awakening',
-      author: 'Kate Chopin',
-    },
-    {
-      id:"2",  
-      title: 'City of Glass',
-      author: 'Paul Auster',
-    },
-    {
-       id:"3",  
-       title: 'Del amor y otros demonios',
-       author: 'Gabriel garcia Marquez',
-    }
-  ];
+let fetchRes = fetch("https://api.breakingbadquotes.xyz/v1/quotes/10");
+let breakingquotes = []
+fetchRes.then(res =>
+    res.json()).then(d => {
+     breakingquotes = d;
+})
+//let breakingquotes = JSON.parse(JSON.stringify([{"quote":"All I can do is wait... for the cancer to come back.","author":"Skyler White"},{"quote":"So you do have a plan? Yeah, Mr. White! Yeah, science!","author":"Jesse Pinkman"},{"quote":"May his death satisfy you.","author":"Gustavo Fring"},{"quote":"Never give up control. Live life on your own terms.","author":"Walter White"},{"quote":"I do not believe fear to be an effective motivator. I want investment. For now, I'm simply interested in time frame","author":"Gustavo Fring"},{"quote":"They're minerals, Marie! Jesus!","author":"Hank Schrader"},{"quote":"You all know exactly who I am. Say my name.","author":"Walter White"},{"quote":"I will put you under the jail.","author":"Hank Schrader"},{"quote":"Walter Jr., you're my big man. There are going to be some things that you'll come to learn about me in the next few days. But just know that no matter how it may look, I only had you in my heart. Goodbye!","author":"Walter White"},{"quote":"Don't drink and drive but if you do, call me.","author":"Saul Goodman"}]));
   const resolvers = {
     Mutation: {
-        CreateBook: (_,arg) => {books.push(arg); return arg},
-        DeleteBook: (_,arg) => { 
-                                 let finalbooks=books.filter(book => book.id != arg.id);
-                                 let bookdeleted = books.find(book => book.id == arg.id );   
-                                 books = [...finalbooks]; 
-                                 return bookdeleted
+        Createbreakingquote: (_,arg) => {breakingquotes.push(arg); return arg},
+        Deletebreakingquote: (_,arg) => { 
+                                 let finalbreakingquotes=breakingquotes.filter(breakingquote => breakingquote.quote != arg.quote);
+                                 let breakingquotedeleted = breakingquotes.find(breakingquote => breakingquote.quote == arg.quote );
+
+            breakingquotes = [...finalbreakingquotes];
+                                 return breakingquotedeleted
                                 },
-        UpdateBook:(_,arg) => {  let objIdx = books.findIndex(book => book.id == arg.id);
-                                 books[objIdx] = arg
-                                 return arg   
-             
+        Updatebreakingquote:(_,arg) => {  let objIdx = breakingquotes.findIndex(breakingquote => breakingquote.quote == arg.quote);
+                                 breakingquotes[objIdx] = arg
+                                 return arg
                               }                        
 
     },  
     Query: {
-      Getbooks: () => books,
-      Getbook: (_,arg) => books.find(number => number.id==arg.id)
+      Getbreakingquotes: () => breakingquotes,
+      Getbreakingquote: (_,arg) => [breakingquotes.find(number => number.quote == arg.quote)]
     },
   };
 
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers ,introspection: true});
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
